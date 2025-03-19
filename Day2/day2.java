9a2acc97e9b7623d1420061a6bdbd2fd79e7f0df
TSS-sniper <tarunsinghsheoran9703@gmail.com>
2025-03-20 00:13:20 +0530

    D1,D2

---
+++ Day2/day2.java
@@ -0,0 +1,39 @@
+package Day2;
+
+public class day2 {
+ static public void main(String [] args) {
+//	 int age =21;
+//	 long num = 7703902553l;
+//	 System.out.println("Insta: abes_memes_department");
+//	 System.out.println("Snap: im._.tss");
+//	 System.out.println(age);
+//	 System.out.println(num);
+	 
+//	 byte a =9;
+//	 byte b =9;
+//	 System.out.println(a+b);
+	 
+	 //reverse a number
+//	 String ans ="";
+//	 int n=123;
+//	 while (n>0) {
+//		ans=ans+n%10;
+//		n=n/10;
+//	}
+//	 System.out.println(ans);
+	 
+	 //rev a num without loop
+//	 String ans ="";
+//	 int n=123;
+//	 ans=ans+n%10;
+//	 n=n/10;
+//	 ans=ans+n%10;
+//	 n=n/10;
+//	 ans=ans+n%10;
+//	 n=n/10;
+//	 System.out.println(ans);
+//	 
+	 System.out.println("20");
+	 System.out.print("240");
+ }
+}
